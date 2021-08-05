d9a026fb9c9b05433e4014de169040ce2a9acf1b
vizvasrj <vizvasrj@gmail.com>
2021-08-05 22:08:55 +0530

    add register to urls.py

--- account/urls.py
+++ account/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('',
         TemplateView.as_view(template_name="account_base.html")),
     path('login/',
-         views.user_login, name='user_login'),
+        views.user_login, name='user_login'),
+    path('register/',
+        views.register, name='register'),
+
 
 ]
