e2e48ff08afeaa0b9f5a286b32bbb15bef168052
vizvasrj <vizvasrj@gmail.com>
2021-08-05 21:33:49 +0530

    create account/templates/account/login.html and edit account/urls.py add\n add user_login views in urlpattern

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+# local
+from . import views
+
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="account_base.html")),
+    path('',
+        TemplateView.as_view(template_name="account_base.html")),
+    path('login/',
+         views.user_login, name='user_login'),
+
 ]
