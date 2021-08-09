8678f222a8ce71668780a94f34c64c37c3cae532
vizvasrj <vizvasrj@gmail.com>
2021-08-09 10:13:47 +0530

    add get_absolute_url to Profiles class of account app add users/ to urls of account app add user_list views to views app

--- account/models.py
+++ account/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
+
+
 from colorfield.fields import ColorField
 
 class Profile(models.Model):
@@ -35,6 +37,9 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user)
+
+    def get_absolute_url(self):
+        return self.user
 
 
 class Contact(models.Model):
--- account/urls.py
+++ account/urls.py
@@ -58,8 +58,10 @@ urlpatterns = [
 
     path('register/',
          views.register, name='register'),
-         
+
     path('edit/', views.edit, name='edit'),
+
+    path('users/', views.user_list, name='user_list'),
 
 
 
