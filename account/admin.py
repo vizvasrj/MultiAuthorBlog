48a62a0812b1228da1100a24e8927fbdff51586d
vizvasrj <vizvasrj@gmail.com>
2021-08-12 01:38:23 +0530

    add Contact model to admin of account app

--- account/admin.py
+++ account/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Profile
 
+from .models import Profile,Contact
 # Register your models here.
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'photo']
     
+
+admin.site.register(Contact)
