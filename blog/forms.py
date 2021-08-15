7fad8af00910f39698a99ed1149de70a28be21fc
vizvasrj <vizvasrj@gmail.com>
2021-08-15 13:52:42 +0530

    blog/forms.py add attribute and class for css

--- blog/forms.py
+++ blog/forms.py
@@ -1,36 +1,63 @@
 # core
-from typing_extensions import Required
 from django import forms
-from django.forms import widgets
 
 # local
-from .models import Post
+from .models import Post, Comment
 
 # 3rd party
 from trumbowyg.widgets import TrumbowygWidget
 from taggit.forms import TagWidget
+from mptt.forms import TreeNodeChoiceField
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = (
-            'title', 'category', 'body', 'tags', 'cover'
+            'title', 'body', 'tags', 'cover'
         )
 
         widgets = {
             'title': forms.TextInput(
-                attrs={'class': 'form-control'}
+                attrs={'class': 'form-control myfieldclass'}
             ),
-            'category': forms.Select(
-                attrs={'class': 'form-control'}
+            'body': TrumbowygWidget(
+                attrs={
+                    'class': 'form-control myfieldclass',
+                }
             ),
-            'body': TrumbowygWidget(),
-            'tags': TagWidget(),
+            'tags': TagWidget(
+                attrs={
+                    'class': 'form-control myfieldclass',
+                }
+            ),
             'cover': forms.FileInput(
                 attrs={
-                    'class': 'form-control',
+                    'class': 'form-control myfieldclass',
                     'required': False
                 }
             )
         }
+
+
+class CommentForm(forms.ModelForm):
+    parent = TreeNodeChoiceField(
+        queryset=Comment.objects.all()
+    )
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['parent'].widget.attrs.update(
+            {'class': 'd-none'}
+        )
+        self.fields['parent'].label = ''
+        self.fields['parent'].required = False
+
+    class Meta:
+        model = Comment
+        fields = ('parent', 'body')
+        widgets = {
+            'body': forms.Textarea()
+        }
+
+    
