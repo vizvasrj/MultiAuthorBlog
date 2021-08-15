0f5d2347dfa26044c268d2295f8b8dff103b5a53
vizvasrj <vizvasrj@gmail.com>
2021-08-15 13:45:32 +0530

    In MultiAuthorBlog/urls.py add blog.url and trumbowyg.urls

--- MultiAuthorBlog/urls.py
+++ MultiAuthorBlog/urls.py
@@ -21,9 +21,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('account.urls')),
-
+    path('blog/', include('blog.urls')),
+    path('trumbowyg/', include('trumbowyg.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
-                            document_root=settings.MEDIA_ROOT)
-
+                          document_root=settings.MEDIA_ROOT)
