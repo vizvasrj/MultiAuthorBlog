9fd564fe91b9f186636103023cf0aaf3ce50f033
vizvasrj <vizvasrj@gmail.com>
2021-08-15 13:47:12 +0530

    format line settings.DEBUG

--- MultiAuthorBlog/urls.py
+++ MultiAuthorBlog/urls.py
@@ -25,5 +25,7 @@ urlpatterns = [
     path('trumbowyg/', include('trumbowyg.urls')),
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
