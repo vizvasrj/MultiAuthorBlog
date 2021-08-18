e4334b449fc4c7d586520165f3245370559454bf
vizvasrj <vizvasrj@gmail.com>
2021-08-19 01:47:43 +0530

    create urls for ( 	create_post, post_detail, post_list, post_search, 	post_like, comment_like, bookmark, update_data, 	delete_post, edit_comment )

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,30 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from .views import (
-    create_post, post_detail, post_list
+    create_post, post_detail, post_list, post_search,
+    post_like, comment_like, bookmark, update_data,
+    delete_post, edit_comment
 )
+
 
 urlpatterns = [
     path('create/', create_post, name='post_new'),
     path('list/', post_list, name='post_list'),
     path('<slug:post>/',
          post_detail, name='post_detail'),
+    path('tag/<slug:tag_slug>/', post_list, 
+        name='post_list_by_tag'),
+    path('search/', post_search, name='post_search'),
+    path('post/like/', post_like, name='like'),
+    path('comment/like/', comment_like, name='comment_like'),
+    path('post/bookmark/', bookmark, name='bookmark'),
+    path('update/<int:pk>/', update_data, name='edit_post'),
+    path('delete/<int:pk>/', delete_post,
+        name='delete_post'),
+    path('404/', TemplateView.as_view(
+        template_name='blog/utility/404.html'
+    ), name='404'),
+    path('update_comment/<int:pk>/', edit_comment,
+        name='edit_comment'),
 ]
