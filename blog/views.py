d751439769acd9933087a0bf1ea8a92a1a99475b
vizvasrj <vizvasrj@gmail.com>
2021-08-19 01:43:33 +0530

    add function ( 	post_search, --to search post 	edit_comment, --to edit comment 	comment_like, --to like comment 	post_like, --to post like 	bookmark, --to bookmark posts 	update_data, --to edit post 	delete_post --to delete post )

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.shortcuts import (
     get_object_or_404, render, redirect
@@ -11,12 +11,18 @@ from django.core.paginator import (
     EmptyPage
 )
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.views.decorators.http import require_POST
+from common.decorators import ajax_required
+from django.contrib.postgres.search import(
+    SearchQuery, SearchVector , SearchRank
+)
 
 
 # local
 from .models import Post, Category, Comment
 from .forms import (
-    PostForm, CommentForm
+    PostForm, CommentForm, SearchForm
 )
 # 3rd party
 from taggit.models import Tag
@@ -123,3 +129,169 @@ def post_detail(request, post):
             'similar_posts': similar_posts
         }
     )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector(
+                'title', 'body'
+            )
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(
+                search=search_vector,
+                rank=SearchRank(
+                    search_vector, search_query
+                )
+            ).filter(search=search_query).order_by('-rank')
+    return render(
+        request,
+        'blog/post/search.html',{
+            'form': form,
+            'query': query,
+            'results': results
+        }
+    )
+
+@login_required
+def edit_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post = get_object_or_404(
+        Post,
+        id=comment.post.id
+    )
+    form = CommentForm(
+        request.POST or None,
+        instance=comment
+    )
+    if request.user.id == comment.commentor_id:
+        if request.method == 'POST':
+            if form.is_valid():
+                comment = form.save(commit=False)
+                comment.post_id = post.id
+                comment.commentor_id = request.user.id
+                comment.save()
+                return redirect(post.get_absolute_url())
+        else:
+            form = CommentForm(instance=comment)
+        return render(
+            request,
+            'blog/comment/update.html',{
+                'form': form,
+                'post': post
+            }
+        )
+    else:
+        return HttpResponseRedirect(reverse('404'))
+
+
+@ajax_required
+@login_required
+@require_POST
+def comment_like(request):
+    comment_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if comment_id and action:
+        try:
+            comment = Comment.objects.get(id=comment_id)
+            if action == 'like':
+                comment.users_like.add(request.user)
+            else:
+                comment.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except:
+            pass
+    return JsonResponse({'status': 'error'})
+
+
+@ajax_required
+@login_required
+@require_POST
+def post_like(request):
+    post_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if post_id and action:
+        try:
+            post = Post.objects.get(id=post_id)
+            if action == 'like':
+                post.users_like.add(request.user)
+            else:
+                post.user_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except:
+            pass
+    return JsonResponse({'status': 'ok'})
+
+
+
+@ajax_required
+@login_required
+@require_POST
+def bookmark(request):
+    post_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if post_id:
+        try:
+            post=Post.objects.get(id=post_id)
+            post.title
+            if action == 'bookmark':
+                post.bookmark_list.add(request.user)
+            else:
+                post.bookmark_list.remove(request.user)
+            return JsonResponse({
+                'title': post.title,
+                'status': 'ok'
+            })
+        except:
+            pass
+    return JsonResponse({'status': 'ok'})
+
+
+
+@login_required
+def update_data(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    form = PostForm(
+        request.POST or None,
+        request.FILES or None,
+        instance=post
+    )
+    if request.user.id == post.author_id:
+        if request.method == 'POST':
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author_id = request.user.id
+                post.save()
+                form.save_m2m()
+                return redirect(post.get_absolute_url())
+        else:
+            form = PostForm(instance=post)
+        return render(
+            request,
+            'blog/post_update.html',
+            {'form': form}
+        )
+    else:
+        return HttpResponseRedirect(reverse('404'))
+
+
+@login_required
+def delete_post(request, pk=None):
+    instance = get_object_or_404(Post, pk=pk)
+    if request.user.id == instance.author_id:
+        instance.delete()
+        return HttpResponseRedirect(
+            reverse('user_detail',args=[
+                request.user.username
+            ])
+        )
+    else:
+        return HttpResponseRedirect(
+            reverse('404')
+        )
+
