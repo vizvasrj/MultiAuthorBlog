73830acf7cce3740edb5135aa6888b29248c64ca
vizvasrj <vizvasrj@gmail.com>
2021-08-20 21:27:41 +0530

    delete ckeditor configs and add ckeditor_uploader

--- MultiAuthorBlog/settings.py
+++ MultiAuthorBlog/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'taggit',
     'mptt',
     'django_editorjs_fields',
+    'ckeditor',
+    'ckeditor_uploader',
     'django_ckeditor_5',
 
     # local
@@ -178,86 +180,14 @@ EDITORJS_DEFAULT_CONFIG_TOOLS = {
         'Table': {'class': 'Table', 'inlineToolbar': True},
     }
 
-# CKeditor-5 [Delete Start ]
-customColorPalette = [
-    {
-        'color': 'hsl(4, 90%, 58%)',
-        'label': 'Red'
-    },
-    {
-        'color': 'hsl(340, 82%, 52%)',
-        'label': 'Pink'
-    },
-    {
-        'color': 'hsl(291, 64%, 42%)',
-        'label': 'Purple'
-    },
-    {
-        'color': 'hsl(262, 52%, 47%)',
-        'label': 'Deep Purple'
-    },
-    {
-        'color': 'hsl(231, 48%, 48%)',
-        'label': 'Indigo'
-    },
-    {
-        'color': 'hsl(207, 90%, 54%)',
-        'label': 'Blue'
-    },
-]
 
-CKEDITOR_5_CUSTOM_CSS = 'path_to.css' # optional
+
+CKEDITOR_UPLOAD_PATH = "uploads/"
+
 CKEDITOR_5_CONFIGS = {
-'default': {
-    'toolbar': ['heading', '|', 'bold', 'italic', 'link',
-                'bulletedList', 'numberedList', 'blockQuote', 'imageUpload', ],
+    'default': {
+        'toolbar': ['heading', '|', 'bold', 'italic', 'link',
+                    'bulletedList', 'numberedList', 'blockQuote', 'imageUpload', ],
 
-},
-'extends': {
-    'blockToolbar': [
-        'paragraph', 'heading1', 'heading2', 'heading3',
-        '|',
-        'bulletedList', 'numberedList',
-        '|',
-        'blockQuote', 'imageUpload'
-    ],
-    'toolbar': ['heading', '|', 'outdent', 'indent', '|', 'bold', 'italic', 'link', 'underline', 'strikethrough',
-    'code','subscript', 'superscript', 'highlight', '|', 'codeBlock',
-                'bulletedList', 'numberedList', 'todoList', '|',  'blockQuote', 'imageUpload', '|',
-                'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor', 'mediaEmbed', 'removeFormat',
-                'insertTable',],
-    'image': {
-        'toolbar': ['imageTextAlternative', 'imageTitle', '|', 'imageStyle:alignLeft', 'imageStyle:full',
-                    'imageStyle:alignRight', 'imageStyle:alignCenter', 'imageStyle:side',  '|'],
-        'styles': [
-            'full',
-            'side',
-            'alignLeft',
-            'alignRight',
-            'alignCenter',
-        ]
-
-    },
-    'table': {
-        'contentToolbar': [ 'tableColumn', 'tableRow', 'mergeTableCells',
-        'tableProperties', 'tableCellProperties' ],
-        'tableProperties': {
-            'borderColors': customColorPalette,
-            'backgroundColors': customColorPalette
-        },
-        'tableCellProperties': {
-            'borderColors': customColorPalette,
-            'backgroundColors': customColorPalette
-        }
-    },
-    'heading' : {
-        'options': [
-            { 'model': 'paragraph', 'title': 'Paragraph', 'class': 'ck-heading_paragraph' },
-            { 'model': 'heading1', 'view': 'h1', 'title': 'Heading 1', 'class': 'ck-heading_heading1' },
-            { 'model': 'heading2', 'view': 'h2', 'title': 'Heading 2', 'class': 'ck-heading_heading2' },
-            { 'model': 'heading3', 'view': 'h3', 'title': 'Heading 3', 'class': 'ck-heading_heading3' }
-        ]
     }
 }
-}
-# CKeditor-5 [Delete END]
