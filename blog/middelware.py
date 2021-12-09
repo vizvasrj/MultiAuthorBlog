46c92339cd18304086c71dbf7c517b883c9e0f51
vizvasrj <vizvasrj@gmail.com>
2021-12-09 09:02:48 +0530

    middleware country code language activate

--- blog/middelware.py
+++ blog/middelware.py
@@ -3,6 +3,7 @@
 # from django.shortcuts import get_object_or_404, redirect
 # from account.models import Profile
 from django.utils import translation
+import requests
 
 def language_change_middleware(get_response):
     # Language Change
@@ -10,7 +11,14 @@ def language_change_middleware(get_response):
         if request.user.is_authenticated:
             translation.activate(request.user.profiles.lang)
         response = get_response(request)
-        # print(request.META.get("REMOTE_ADDR"))
+        ip = request.META.get("REMOTE_ADDR")
+        response = requests.get(f'http://ip-api.com/csv/{ip}?fields=countryCode')
+        text = response.text
+        country_code = text.split('\n')[0]
+        if country_code == 'IN':
+            translation.activate('hi')
+        elif country_code == 'DE':
+            translation.activate('de')
         return response
     return middleware
 
