7e63a923ac1110f9135b7963481fa64e7800f045
vizvasrj <vizvasrj@gmail.com>
2021-12-09 09:47:35 +0530

    what error us/us

--- blog/middelware.py
+++ blog/middelware.py
@@ -11,17 +11,17 @@ def language_change_middleware(get_response):
         if request.user.is_authenticated:
             translation.activate(request.user.profiles.lang)
         response = get_response(request)
-        ip = request.META.get("REMOTE_ADDR")
+        ip = str(request.META.get("REMOTE_ADDR"))
         curl = requests.get(f'http://ip-api.com/csv/{ip}?fields=countryCode')
         text = curl.text
         country_code = text.split('\n')[0]
-        translation.activate('country_code')
+        translation.activate(country_code)
         # if country_code == 'IN':
         # elif country_code == 'DE':
         #     translation.activate('de')
         # else:
         #     translation.activate('fr')
-        print(text, country_code, "text and &&& country code")
+        # print(text, country_code, "text and &&& country code")
         
         return response
     return middleware
