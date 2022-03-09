2a36b74a33e4d85b8b2ca87729dd9cfee3fddb75
vizvasrj <vizvasrj@gmail.com>
2022-03-10 03:35:00 +0530

    userdone3

--- blog/rest_views.py
+++ blog/rest_views.py
@@ -540,7 +540,7 @@ class RandomPostCRUDView(generics.ListCreateAPIView):
 
         p = queryset[0]
         # p = Post.objects.all().order_by("-created")[0]
-        rp = r.smembers("usedone2")
+        rp = r.smembers("usedone3")
         # print(rp)
         stringlist = [x.decode("utf-8") for x in rp]
         # print(stringlist)
@@ -554,7 +554,7 @@ class RandomPostCRUDView(generics.ListCreateAPIView):
                 ~Q(id__in=set_list)
             )[0]
         # print(p)
-        r.sadd("usedone2", p.id)
+        r.sadd("usedone3", p.id)
         queryset = Post.objects.all().filter(id=p.id)
         
         page = self.paginate_queryset(queryset)
